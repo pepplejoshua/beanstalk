47107e293ddbbe5c26a665c0d2b824bb4547a5eb
Joshua Pepple <pepplejoshuatams@gmail.com>
2024-01-08 23:35:45 -0700

    Add prisma server wrapper and landing page after successful auth

---
+++ app/routes/home.tsx
@@ -0,0 +1,9 @@
+export default function GbfHome() {
+  return (
+    <div className="w-full">
+      <h1 className="text-6xl font-bold text-center">
+        welcome to gbf
+      </h1>
+    </div>
+  )
+}
