6cb53a35e855a3860f1da8a7d7062d1146090e66
Joshua Pepple <pepplejoshuatams@gmail.com>
2024-01-10 23:39:56 -0700

    Refactor user profile page and add user edit functionality

--- app/routes/users._index/user.tsx
+++ app/routes/users._index/user.tsx
@@ -12,25 +12,26 @@ import type { AuthenticatedUser } from "~/services/auth.server";
 // TODO(@sewedur): style this nicely
 export default function User({user}: {user: AuthenticatedUser}) {
   return (
-    <div className="mt-5 flex flex-row">
+    <div className="ml-5 mt-5 flex flex-row border-2 rounded-lg px-5 py-3">
       <div className="w-2/3 flex flex-col">
         <h1 className="text-3xl font-bold">
           {user.first_name}, {user.last_name}
+          <span className="text-2xl font-bold text-gray-400 ml-2">
+            @{user.username}
+          </span>
         </h1>
-        <h2 className="text-2xl font-bold">
-          .{user.username}
+        <h2 className="">
+          
         </h2>
-        <h3 className="text-xl font-bold">
-          {user.email}
-        </h3>
-        <h4 className="text-lg font-bold">
+    
+        <span className="text-lg font-bold text-gray-600">
           {user.company_role}
-        </h4>
+        </span>
       </div>
 
       {/* add a section for editing a user */}
-      <div className="w-1/3">
-        <Link to={`edit/${user.id}`} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      <div className="w-1/3 items-center flex flex-row justify-end">
+        <Link to={`edit/${user.username}`} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
           Edit User
         </Link>
       </div>
---
+++ app/routes/users.edit.$username/route.tsx
@@ -0,0 +1,184 @@
+import type { ActionFunctionArgs, LoaderFunctionArgs } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import invariant from "tiny-invariant";
+import BeanLayout from "~/components/bean_layout";
+import { authenticator } from "~/services/auth.server";
+import { prisma } from "~/services/prisma.server";
+
+export default function EditUser() {
+  let info_bunder = useLoaderData<typeof loader>();
+  let { user, editingUser, fatalError } = info_bunder;
+
+  let editingSelf = user.id === editingUser?.id;
+
+  return (
+    <BeanLayout>
+      <h1 className="text-5xl font-bold text-center">
+        {/* show this if there is a fatalError */}
+        { fatalError && 
+          <p className="font-bold text-center text-5xl">error</p>  }
+        {/* show this if there is no fatalError */}
+        { !fatalError && 
+          <p className="text-5xl font-bold text-center mb-10">
+            {editingSelf ? "edit your profile" : `edit ${editingUser?.username}'s profile`}
+          </p>
+        }
+      </h1>
+
+      { fatalError &&
+          <div className="mt-20">
+            <p className="text-red-500 text-2xl text-center">
+              {fatalError}
+            </p>
+          </div>  
+      }
+
+      { !fatalError && 
+          <div className="mt-10 mx-auto w-3/4">
+            <form className="flex flex-col text-justify" method="POST" action="/users/edit">
+              <label htmlFor="first_name" className="text-3xl font-bold mb-2">
+                first name
+              </label>
+              <input
+                type="text"
+                name="first_name"
+                id="first_name"
+                defaultValue={editingUser?.first_name}
+                className="border-2 border-gray-500 rounded-lg p-2 mb-5"
+              />
+
+              <label htmlFor="last_name" className="text-3xl font-bold mb-2">
+                last name
+              </label>
+              <input
+                type="text"
+                name="last_name"
+                id="last_name"
+                defaultValue={editingUser?.last_name}
+                className="border-2 border-gray-500 rounded-lg p-2 mb-5"
+                required
+              />
+
+              <label htmlFor="username" className="text-3xl font-bold mb-2">
+                username
+              </label>
+              <input
+                type="text"
+                name="username"
+                id="username"
+                defaultValue={editingUser?.username}
+                className="border-2 border-gray-500 rounded-lg p-2 mb-5"
+                required
+              />
+
+              <label htmlFor="email" className="text-3xl font-bold mb-2">
+                email
+              </label>
+              <input
+                type="text"
+                name="email"
+                id="email"
+                defaultValue={editingUser?.email}
+                className="border-2 border-gray-500 rounded-lg p-2 mb-5"
+                required
+              />
+
+              <label htmlFor="company_role" className="text-3xl font-bold mb-2">
+                company role
+              </label>
+              <input
+                type="text"
+                name="company_role"
+                id="company_role"
+                defaultValue={editingUser?.company_role}
+                className="border-2 border-gray-500 rounded-lg p-2 mb-5"
+                required
+              />
+
+              {/* TODO(@pepplejoshua): add optional password field here. style fields like gbf page */}
+
+              <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                submit
+              </button>
+            </form>
+          </div>
+      }
+    </BeanLayout>
+  )
+}
+
+export async function loader({request, params}: LoaderFunctionArgs) {
+  let user = await authenticator.isAuthenticated(request, {
+    failureRedirect: '/gbf',
+  });
+
+  let editingUsername = params.username;
+  let editingUser = await prisma.user.findUnique({
+    where: {
+      username: editingUsername,
+    }
+  });
+
+  let fatalError = undefined;
+  if (!editingUser) {
+    fatalError = "this user was not found";
+  }
+
+  return {
+    user,
+    editingUser,
+    fatalError,
+  }
+}
+
+export async function action({request, params}: ActionFunctionArgs) {
+  let updatingUser = params.username;
+
+  let formData = await request.formData();
+  let error = undefined;
+
+  let first_name = formData.get("first_name");
+  invariant(typeof first_name === "string", "first name must be a string");
+  let last_name = formData.get("last_name");
+  invariant(typeof last_name === "string", "last name must be a string");
+  let username = formData.get("username");
+  invariant(typeof username === "string", "username must be a string");
+  let email = formData.get("email");
+  invariant(typeof email === "string", "email must be a string");
+  let regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  let is_email = regex.test(email);
+  if (!is_email) {
+    error = "email must be a valid email";
+  }
+  let company_role = formData.get("company_role");
+  invariant(typeof company_role === "string", "company role must be a string");
+
+  if (error) {
+    return new Response(error, {
+      status: 401,
+    });
+  }
+
+  let user = await prisma.user.update({
+    data: {
+      first_name,
+      last_name,
+      username,
+      email,
+      company_role,
+    },
+    where: {
+      username: updatingUser,
+    }
+  });
+
+  if (!user) {
+    error = "this user was not found";
+  }
+
+  if (error) {
+    return new Response(error, {
+      status: 401,
+    });
+  }
+}
